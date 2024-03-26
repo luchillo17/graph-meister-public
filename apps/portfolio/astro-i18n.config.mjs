73f724c1950f6abbde0baeeaf9f425a12a44c200
Carlos Esteban Lopez Jaramillo <luchillo17@gmail.com>
2024-03-26 00:10:56 -0500

    fix: Reposition extracted i18n folder names

--- apps/portfolio/astro-i18n.config.mjs
+++ apps/portfolio/astro-i18n.config.mjs
@@ -7,8 +7,7 @@ export default defineAstroI18nConfig({
   trailingSlash: 'never', // "never" or "always"
   run: 'client+server', // "client+server" or "server"
   showPrimaryLocale: false, // "/en/about" vs "/about"
-  translationLoadingRules: [], // per page group loading
-  translationDirectory: {}, // translation directory names
-  translations: {}, // { [translation_group1]: { [locale1]: {}, ... } }
-  routes: {}, // { [secondary_locale1]: { about: "about-translated", ... } }
+  translationDirectory: {
+    pages: '',
+  }, // translation directory names
 });
